778ae6c34d6beb5be3c7030b2c394e9144ff2d3e
ZCKHGL <zacky.haigel0505@gmail.com>
2025-10-16 10:00:44 +0700

    Praktikum 4

--- data_kelurahan/warga/urls.py
+++ data_kelurahan/warga/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import WargaListView, WargaDetailView, PengaduanListView, WargaCreateView, PengaduanCreateView
+from .views import WargaListView, WargaDetailView, PengaduanListView, WargaCreateView, PengaduanCreateView, WargaUpdateView, WargaDeleteView, PengaduanUpdateView, PengaduanDeleteView
 
 urlpatterns = [
     path('warga/',WargaListView.as_view(), name='warga_list'),
@@ -7,4 +7,8 @@ urlpatterns = [
     path('pengaduan/', PengaduanListView.as_view(), name='pengaduan_list'),
     path('pengaduan/tambah/', PengaduanCreateView.as_view(), name='pengaduan_add'),
     path('warga/add/', WargaCreateView.as_view(), name='warga_add'),
+    path('warga/<int:pk>/edit/', WargaUpdateView.as_view(), name='warga_edit'),
+    path('warga/<int:pk>/delete/', WargaDeleteView.as_view(), name='warga_delete'),
+    path('pengaduan/<int:pk>/edit/', PengaduanUpdateView.as_view(), name='pengaduan_edit'),
+    path('pengaduan/<int:pk>/delete/', PengaduanDeleteView.as_view(), name='pengaduan_delete'),
 ]
--- data_kelurahan/warga/views.py
+++ data_kelurahan/warga/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Warga, Pengaduan
 from django.urls import reverse_lazy
 from .forms import WargaForm, PengaduanForm
@@ -26,3 +28,62 @@ class PengaduanCreateView(CreateView):
     form_class = PengaduanForm
     template_name = 'warga/pengaduan_form.html'
     success_url = reverse_lazy('pengaduan_list')
+
+class WargaUpdateView(UpdateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Data warga berhasil diperbarui.')
+        return response
+
+class WargaDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Warga
+    template_name = 'warga/warga_confirm_delete.html'
+    success_url = reverse_lazy('warga_list')
+
+    def test_func(self):
+        # only staff users can delete
+        return self.request.user.is_staff
+
+    def handle_no_permission(self):
+        messages.error(self.request, 'Anda tidak memiliki izin untuk menghapus data warga.')
+        return super().handle_no_permission()
+
+    def delete(self, request, *args, **kwargs):
+        obj = self.get_object()
+        messages.success(request, f"Data warga '{obj.nama_lengkap}' berhasil dihapus.")
+        return super().delete(request, *args, **kwargs)
+
+
+class PengaduanUpdateView(UpdateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Pengaduan berhasil diperbarui.')
+        return response
+
+
+class PengaduanDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_confirm_delete.html'
+    success_url = reverse_lazy('pengaduan_list')
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+    def handle_no_permission(self):
+        messages.error(self.request, 'Anda tidak memiliki izin untuk menghapus pengaduan.')
+        return super().handle_no_permission()
+
+    def delete(self, request, *args, **kwargs):
+        obj = self.get_object()
+        messages.success(request, f"Pengaduan '{obj.judul}' berhasil dihapus.")
+        return super().delete(request, *args, **kwargs)
